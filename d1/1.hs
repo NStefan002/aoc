ace1e998b86388e3aedb9136274a542c6f2a6303
NStefan002 <n.stefan002@gmail.com>
2024-12-17 15:28:27 +0100

    day 1

---
+++ d1/1.hs
@@ -0,0 +1,27 @@
+import Data.List (sort)
+import System.IO
+
+split :: Char -> String -> [String]
+split _ "" = []
+split c s = first : split c rest
+  where
+    first = takeWhile (/= c) s
+    rest = drop (length first + 1) s
+
+getOddIndexElements :: [Int] -> [Int]
+getOddIndexElements [] = []
+getOddIndexElements numbers = [x | (x, i) <- zip numbers [0 ..], odd i]
+
+getEvenIndexElements :: [Int] -> [Int]
+getEvenIndexElements [] = []
+getEvenIndexElements numbers = [x | (x, i) <- zip numbers [0 ..], even i]
+
+calculateDistances :: [Int] -> [Int] -> Int
+calculateDistances [] [] = 0
+calculateDistances (ah : at) (bh : bt) = abs (ah - bh) + calculateDistances at bt
+
+main :: IO ()
+main = do
+    contents <- readFile "input.txt"
+    let numbers = map read (words contents) :: [Int]
+    print $ calculateDistances (sort (getEvenIndexElements numbers)) (sort (getOddIndexElements numbers))
---
+++ d1/2.hs
@@ -0,0 +1,27 @@
+import Data.List (sort)
+import System.IO
+
+split :: Char -> String -> [String]
+split _ "" = []
+split c s = first : split c rest
+  where
+    first = takeWhile (/= c) s
+    rest = drop (length first + 1) s
+
+getOddIndexElements :: [Int] -> [Int]
+getOddIndexElements [] = []
+getOddIndexElements numbers = [x | (x, i) <- zip numbers [0 ..], odd i]
+
+getEvenIndexElements :: [Int] -> [Int]
+getEvenIndexElements [] = []
+getEvenIndexElements numbers = [x | (x, i) <- zip numbers [0 ..], even i]
+
+calculateSimilarityScore :: [Int] -> [Int] -> Int
+calculateSimilarityScore [] _ = 0
+calculateSimilarityScore (ah : at) b = ah * length (filter (== ah) b) + calculateSimilarityScore at b
+
+main :: IO ()
+main = do
+    contents <- readFile "input.txt"
+    let numbers = map read (words contents) :: [Int]
+    print $ calculateSimilarityScore (getEvenIndexElements numbers) (getOddIndexElements numbers)
