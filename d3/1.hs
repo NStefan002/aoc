08bf8f1c5393423a3a19b1d6d055ebd822add740
NStefan002 <n.stefan002@gmail.com>
2024-12-19 01:32:56 +0100

    day 3

---
+++ d3/1.hs
@@ -0,0 +1,8 @@
+import Text.Regex.TDFA
+
+main :: IO ()
+main = do
+    contents <- readFile "input.txt"
+    let pattern = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)"
+    let matches = contents =~ pattern :: [[String]]
+    print $ sum $ map (\m -> (read (m !! 1) :: Int) * (read (m !! 2) :: Int)) matches
---
+++ d3/2.hs
@@ -0,0 +1,20 @@
+import Text.Regex.TDFA
+
+result' :: Bool -> [[String]] -> Int
+result' _ [] = 0
+result' on (h : t)
+    | what == "do()" = result' True t
+    | what == "don't()" = result' False t
+    | otherwise = if on then ((read (h !! 1) :: Int) * (read (h !! 2) :: Int)) + result' on t else result' on t
+  where
+    what = head h
+
+result :: [[String]] -> Int
+result = result' True
+
+main :: IO ()
+main = do
+    contents <- readFile "input.txt"
+    let pattern = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)|do\\(\\)|don't\\(\\)"
+        matches = contents =~ pattern :: [[String]]
+    print $ result matches
